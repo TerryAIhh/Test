62f1a6a69f8b5b4addf3ba7aa89fb380f81b09ed
TerryAIhh <56039040+terryaihh@users.noreply.github.com>
2021-05-31 23:03:44 +0800

    ing

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-# Press the green button in the gutter to run the script.
+def screen_cut():
+    driver = webdriver.Chrome()
+    driver.get("https://mms.pinduoduo.com/orders/list")
+    filename = "image.png"
+    # 获取需要点击的元素
+    element = driver.find_element(By.XPATH,
+                                  '//*[@id="mf-mms-orders-container"]/div/div/div/div[5]/div[2]/div/div[1]/div['
+                                  '2]/div[2]/div/a/span')
+    element.click()
+    # 截屏
+    driver.save_screenshot(filename)
+    # 获取元素上下左右的位置
+    left = element.location['x']
+    top = element.location['y']
+    right = element.location['x'] + element.size['width']
+    bottom = element.location['y'] + element.size['height']
+
+    # 打开刚才的截图
+    im = Image.open(filename)
+    # 截取对应位置
+    im = im.crop((left, top, right, bottom))
+    # 保存覆盖原有截图
+    im.save(filename)
+
+    driver.quit()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    screen_cut()
